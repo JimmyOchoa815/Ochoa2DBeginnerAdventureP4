6e1c261baf9d41317ff6f4f38897fce18ff761c7
JimmyOchoa815 <768328@student.dusd.net>
2025-12-04 12:10:57 -0800

    added many more stuff
    added decorations and more tiles and characters

--- Ochoa2DBeginnerAdventureP4/Assets/Scripts/PlayerController.cs
+++ Ochoa2DBeginnerAdventureP4/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,10 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     
+    
     public InputAction MoveAction;
+
+
     // Start is called before the first frame update
     void Start()
     {
